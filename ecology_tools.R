5860ad05ac67ad48a07d1bf7bc778fdadbb07cfc
KYLE DOHERTY <kyledoherty@KYLEs-iMac-Pro.local>
2021-03-15 15:49:20 -0700

    tidy dev

--- ecology_tools.R
+++ ecology_tools.R
@@ -10,16 +10,21 @@ library(scales)
 
 source_app <- function(local = FALSE, dev = FALSE) {
   if(isTRUE(local)){
+  source('/Users/kyledoherty/mpgPostdoc/projects/ecology_tools/app/global.R')
   source('/Users/kyledoherty/mpgPostdoc/projects/ecology_tools/app/ui.R')
   source('/Users/kyledoherty/mpgPostdoc/projects/ecology_tools/app/server.R')
-  source('/Users/kyledoherty/mpgPostdoc/projects/ecology_tools/app/global.R')
     }
   if(isFALSE(local) & isFALSE(dev)){
+    source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/main/global.R') #remote global
     source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/main/ui.R') #remote ui
     source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/main/server.R') #remote server
-    source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/main/global.R') #remote global
   }
   
+  if(isFALSE(local) & isTRUE(dev)){
+    source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/dev/global.R') #remote global
+    source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/dev/ui.R') #remote ui
+    source('https://raw.githubusercontent.com/mosscoder/mpg_ecology_tools/dev/server.R') #remote server
+  }
 }
 
 source_app(local = FALSE, dev = TRUE)
